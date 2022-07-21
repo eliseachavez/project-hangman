31dc9e19e64a2b24583ee4e98d9e569db25394dc
lemur <noctiphobic@gmail.com>
2022-07-21 16:14:29 -0500

    Add methods and change initialization to allow for a game to be serialized, saved, and reloaded

--- lib/game.rb
+++ lib/game.rb
@@ -1,29 +1,54 @@
 require_relative "display.rb"
+require 'yaml'
+
 class Game
   include Display
 
-  def initialize(dictionary=[], word=nil)
-    @dictionary = []
-    @word = choose_word
+  def initialize(dictionary=[], word_in_progress=[], past_guesses=[], word=nil)
+    @dictionary = dictionary
+    @word_in_progress = word_in_progress
+    @past_guesses = past_guesses
+    @word = word
     play_game
   end
 
   def play_game
-    print_greeting
-
+    choose_word
+    #tell them the progress of their word
+    #tell them what they've already guessed
+    #tell them how many guesses left
   end
 
   def choose_word
-    unfiltered_dictionary = File.open("google-10000-english-no-swears.txt", 'r').readlines.each do |line|
-      @dictionary.push(line.chomp) if line.chomp.length > 5
+    if word == nil
+      unfiltered_dictionary = File.open("google-10000-english-no-swears.txt", 'r').readlines.each do |line|
+        @dictionary.push(line.chomp) if line.chomp.length > 5
+      end
+      rand_num = generate_rand_num(@dictionary.length)
+      @dictionary[rand_num]
+    else
+      @word
     end
-    rand_num = generate_rand_num(@dictionary.length)
-    @dictionary[rand_num]
   end
 
   def generate_rand_num(max_size)
     rand_num = rand(max_size)
   end
 
+  def to_yaml
+    #turn obj into a hash that is serialized with YAML
+    YAML.dump({
+      :dictionary => @dictionary
+      :word_in_progress => @word_in_progress
+      :past_guesses = @past_guesses
+      :word = @word
+    })
+  end
+
+  def self.from_yaml(string)
+    #turn string hash into obj
+    data = YAML.load string
+    self.new(data[:dictionary], data[:word_in_progress], data[:past_guesses], data[:word])
+  end
 
 end
