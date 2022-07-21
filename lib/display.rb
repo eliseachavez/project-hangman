c021d9ae91f84aa03c159f8c04d7241926f65493
lemur <noctiphobic@gmail.com>
2022-07-21 15:15:37 -0500

    Write logic for high level game flow

--- lib/display.rb
+++ lib/display.rb
@@ -1,5 +1,19 @@
 module Display
+  def ask_new_or_saved_game?
+    puts "Would you like to play a new game or a saved one?"
+  end
+
   def print_greeting
     puts "The game has begun; welcome to Hangman"
+  end
+
+  def ask_to_continue
+    puts "Would you like to play another game?\nType y for yes and n for no.\n"
+    reply = gets.chomp.downcase
+    if reply == y
+      true
+    else
+      false
+    end
   end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,25 @@
 require_relative 'game.rb'
+require_relative 'display.rb'
 
-game1 = Game.new
+include Display
+
+def playing
+  puts "Would you like to play a new or saved game?\nType y for yes and n for no.\n"
+  reply = gets.chomp.downcase
+  if reply == y
+    true
+  else
+    false
+  end
+end
+
+while playing
+  option = ask_new_or_saved_game?
+  if option == 1
+    game1 = Game.new
+  elsif option == 2
+    pull_up_saved_games
+  else
+    puts "That wasn't a valid option"
+  end
+end
